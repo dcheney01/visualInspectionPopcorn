5ef0819f5edcd2717633fa4ef56104d49c0e73a8
Daniel Cheney <daniel.cheney01@gmail.com>
2024-01-25 15:16:38 -0700

    updated inspection script

--- src/run_inspection.py
+++ src/run_inspection.py
@@ -1,26 +1,13 @@
 import time
 from ultralytics import YOLO
 import cv2 as cv
-import numpy as np
 
-'''
-Set WEBCAM to 1 to use your webcam or 0 to use the Flea2 cameras on the lab machine
-Set CATCHER to 1 to use the catcher connected to the lab machine or 0 to use your own computer
-'''
-WEBCAM = 1
-
-if WEBCAM:
-    camera = cv.VideoCapture(0)
-else:
-    from src.Flea2Camera import FleaCam
-    camera = FleaCam()
-    camera.set
+camera = cv.VideoCapture(0)
 
 def main(weights_path):
     model = YOLO(weights_path)
 
     while True:
-
         if WEBCAM:
             ret0, frame = camera.read()
         else:
