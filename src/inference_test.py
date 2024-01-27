adbc5ae0658d61b73ba27e6c48175fa21eda03d4
Daniel Cheney <daniel.cheney01@gmail.com>
2024-01-27 09:23:40 -0700

    finished running inference tests

--- src/inference_test.py
+++ src/inference_test.py
@@ -3,22 +3,22 @@ import os
 import time
 import cv2
 
-model = YOLO('/home/daniel/software/visualInspectionPopcorn/runs/detect/train3/weights/best.pt')
-data_folder = "/home/daniel/software/visualInspectionPopcorn/data/annotated_data/webcam/images"
+model = YOLO('/fsg/dcheney1/visualInspectionPopcorn/runs/detect/train3/weights/best.pt')
+data_folder = "/media/dcheney1/CCJ/popcorn_classification/raw_data/webcam"
 
+print("loaded model")
 cv2.namedWindow("YOLO Detection", cv2.WINDOW_NORMAL)
-images = sorted(os.listdir(data_folder))
 
-for img in images:
-    img_path = os.path.join(data_folder, img)
-    image = cv2.imread(img_path)
+for folder in os.listdir(data_folder):
+    folder_path = os.path.join(data_folder, folder)
+    for img in os.listdir(folder_path):
+        img_path = os.path.join(folder_path, img)
+        image = cv2.imread(img_path)
 
-    start = time.time()
-    results = model.predict(image, show=True, verbose=False, conf=0.8)
-    end = time.time()
+        start = time.time()
+        results = model.predict(image, show=True, verbose=False, conf=0.8)
+        end = time.time()
 
-    print(f"Time: {end - start}")
+        print(f"Time: {end - start}")
 
-    cv2.waitKey(0)
-
-
+        cv2.waitKey(0)
--- src/run_inspection.py
+++ src/run_inspection.py
@@ -1,24 +1,44 @@
 import time
 from ultralytics import YOLO
 import cv2 as cv
+import numpy as np
+from PIL import Image
+
+# def main(weights_path):
+weights_path = "/fsg/dcheney1/visualInspectionPopcorn/runs/detect/train3/weights/best.pt"
+video_writer = cv.VideoWriter("output_video.mp4", cv.VideoWriter_fourcc(*"MJPG"),
+                                10, (640,480))
 
 camera = cv.VideoCapture(0)
+# camera.set(cv.CAP_PROP_BRIGHTNESS, 8)
+# camera.set(cv.CAP_PROP_EXPOSURE, 20)
+# camera.set(cv.CAP_PROP_CONTRAST, 10)
+# camera.set(cv.CAP_PROP_FPS, 60)
 
-def main(weights_path):
-    model = YOLO(weights_path)
+print("Camera Started...")
 
-    while True:
-        if WEBCAM:
-            ret0, frame = camera.read()
-        else:
-            frame = camera.getFrame()
+model = YOLO(weights_path)
+print("Model Loaded...")
 
-        start = time.time()
-        result = model.predict(source=frame, imgsz=640, show=True, verbose=False, conf=0.3, device="cpu")
-        end = time.time()
-        print(f"Loop Time: {end-start}")
+counter = 0
+
+while True:
+    ret0, frame = camera.read()
+
+    start = time.time()
+    result = model.predict(source=frame, imgsz=640, show=True, verbose=False, conf=0.75)
+    end = time.time()
+    print(f"Loop Time: {end-start}")
+
+    # Show the results
+    for r in result:
+        im_array = r.plot()  # plot a BGR numpy array of predictions
+        im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+        im.save(f'simple_video/{counter}.jpg')  # save image
+
+    counter += 1
 
 
-if __name__ == "__main__":
-    weights_path = "/fsg/dcheney1/visualInspectionPopcorn/runs/detect/train/weights/best.pt"
-    main(weights_path)
+# if __name__ == "__main__":
+#     print
+#     main(weights_path)
